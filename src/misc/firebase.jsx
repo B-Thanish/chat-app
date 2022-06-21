12e94f0bc86cb3cd877d2f21d1300fb9af0ad5a7
B-Thanish <banothuthanish@gmail.com>
2022-06-21 14:00:44 +0530

    uploaded user avatar

--- src/misc/firebase.jsx
+++ src/misc/firebase.jsx
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
+import 'firebase/storage';
 
 const config = {
   apiKey: 'AIzaSyAtshz1gL5-fi_6q7UxRdTUBB7HfEHbNSU',
@@ -15,3 +16,4 @@ const app = firebase.initializeApp(config);
 
 export const auth = app.auth();
 export const database = app.database();
+export const storage = app.storage();
