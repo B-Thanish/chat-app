7bb23ced4224e84969c26e538139d3f21f4da0e8
B-Thanish <banothuthanish@gmail.com>
2022-06-21 21:35:12 +0530

    created layout for chat page

---
+++ src/components/chat-window/bottom/index.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable react/function-component-definition */
+import React from 'react';
+
+const Bottom = () => {
+  return <div>bottom</div>;
+};
+
+export default Bottom;
---
+++ src/components/chat-window/top/index.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable react/function-component-definition */
+import React from 'react';
+
+const Top = () => {
+  return <div>top</div>;
+};
+
+export default Top;
