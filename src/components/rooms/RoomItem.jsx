681d13c5ac9d3949f41e9226364f20c4c9d6316a
B-Thanish <banothuthanish@gmail.com>
2022-06-21 17:23:59 +0530

    started to work with chat room list

---
+++ src/components/rooms/RoomItem.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/function-component-definition */
+import React from 'react';
+import TimeAgo from 'timeago-react';
+
+const RoomItem = () => {
+  return (
+    <div>
+      <div className="d-flex justify-content-between align-items-center">
+        <h3 className="text-text-disappear ">Room name</h3>
+        <TimeAgo datatime={new Date()} className="font-normal text-black-45" />
+      </div>
+
+      <div className="d-flex align-items-center text-black-70">
+        <span>No messages yet...</span>
+      </div>
+    </div>
+  );
+};
+
+export default RoomItem;
