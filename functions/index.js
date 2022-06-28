1806bdbb520e5c05ecec7a8044d281e70de7b9a4
B-Thanish <banothuthanish@gmail.com>
2022-06-28 19:33:25 +0530

    added cloud function to send fcm messages

---
+++ functions/index.js
@@ -0,0 +1,11 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const serviceAccount = require('./service-account.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://chat-web-app-97787-default-rtdb.firebaseio.com',
+});
+
+const { sendFcm } = require('./src/fcm');
+exports.sendFcm = sendFcm;
